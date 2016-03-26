721cf8fff6e1cdbd7913e8edd6cbb2551a712b29
bindai <bindai@indiana.edu>
2016-03-25 21:37:10 -0700

    add mock data and create left-nav component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import LeftNav from './left-nav';
+
+const data = [
+  {
+    title: "工程研发部门",
+    jobs: [
+      {
+        title: "Mac开发工程师",
+        count: 9
+      }, {
+        title: "iOS App测试工程师",
+        count: 17
+      }, {
+        title: "Android 远程控制工程师",
+        count: 61
+      }, {
+        title: "Web 前端工程师",
+        count: 31
+      }, {
+        title: "Android 多媒体软件开发工程师",
+        count: 2
+      }
+    ]
+  }, {
+    title: "产品设计部门",
+    jobs: [
+      {
+        title: "网页设计师",
+        count: 47
+      }, {
+        title: "ID / 工业设计师",
+        count: 39
+      }, {
+        title: "视觉设计师 / GUI 界面设计师",
+        count: 42
+      }, {
+        title: "平面设计师",
+        count: 8
+      }
+    ]
+  }
+];
 
 const App = React.createClass({
 
   render() {
     return (
-        <p>Hello React!</p>
+        <LeftNav data={data}/>
     );
   }
 });
---
+++ src/components/left-nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const title = "招聘职位";
+
+const LeftNav = React.createClass({
+
+  render() {
+    return (
+      <div>
+        {title}
+      </div>
+    );
+  }
+});
+
+export default LeftNav;
