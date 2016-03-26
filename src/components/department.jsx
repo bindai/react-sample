aea06443080b15f944fc092c209af42ed6ed930f
bindai <bindai@indiana.edu>
2016-03-25 23:47:05 -0700

    add expand/collapse feature in department component

--- src/components/department.jsx
+++ src/components/department.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import Job from './job';
+import {FaAngleDown, FaAngleUp} from 'react-icons/lib/fa';
 
 const styles = {
   department: {
@@ -17,10 +18,18 @@ const styles = {
     float: 'left'
   },
   title: {
+    float: 'left',
     color: '#FFFFFF',
     fontSize: '13px',
     fontFamily: 'Sans-serif',
     fontWeight: 'bold'
+  },
+  iconArea: {
+    float: 'left',
+    paddingLeft: '3%'
+  },
+  icon: {
+    color: '#FFFFFF'
   },
   countArea: {
     width: '10%',
@@ -47,7 +56,13 @@ const Department = React.createClass({
   },
 
   getInitialState() {
-    return {hasSelectedAll: false};
+    return {hasSelectedAll: false, expand: true};
+  },
+
+  handleExpand() {
+    this.setState({
+      expand: !this.state.expand
+    })
   },
 
   handleSelectChange(event) {
@@ -63,19 +78,26 @@ const Department = React.createClass({
   render() {
     let jobs = this.props.department.jobs.map((item, index) => <Job key={index} job={item} isSelected={this.state.hasSelectedAll}/>);
     let sum = this.props.department.jobs.map((item) => item.count).reduce((prev, curr) => prev + curr);
+    let lists = this.state.expand
+      ? jobs
+      : '';
+    let icon = this.state.expand
+      ? <FaAngleUp key={0} style={styles.icon}/>
+      : <FaAngleDown key={1} style={styles.icon}/>
 
     return (
       <div style={styles.department}>
         <div style={styles.select}>
           <input type="checkbox" style={styles.checkbox} checked={this.state.hasSelectedAll} onChange={this.handleSelectChange}/>
         </div>
-        <div style={styles.titleArea}>
+        <div style={styles.titleArea} onClick={this.handleExpand}>
           <div style={styles.title}>{this.props.department.title}</div>
+          <div style={styles.iconArea}>{icon}</div>
         </div>
         <div style={styles.countArea}>
           <div style={styles.count}>{sum}</div>
         </div>
-        {jobs}
+        {lists}
       </div>
     );
   }
